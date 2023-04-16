cfb70b88c28d02e05684c75f0393de16e26a3232
Jan Bureš <jan.bures@live.com>
2023-04-16 03:36:02 +0200

    Nuget packaging info + switch from dll references to packages

--- UitkPatcherProject/Patcher.cs
+++ UitkPatcherProject/Patcher.cs
@@ -6,14 +6,15 @@ namespace UITKPatcher;
 public static class Patcher
 {
     private static readonly string BasePath = Path.Combine("BepInEx", "plugins", Ksp2UitkPlugin.ModFolder, "lib");
-    
+
     public static IEnumerable<string> TargetDLLs { get; } = new[]
     {
+        "UnityEngine.TextCoreTextEngineModule.dll",
         "UnityEngine.UI.dll",
+        "UnityEngine.UIElementsInputSystemModule.dll",
         "UnityEngine.UIElementsModule.dll",
         "UnityEngine.UIElementsNativeModule.dll",
-        "UnityEngine.UIModule.dll",
-        "UnityEngine.TextCoreTextEngineModule.dll",
+        "UnityEngine.UIModule.dll"
     };
 
     public static void Patch(ref AssemblyDefinition assembly)
