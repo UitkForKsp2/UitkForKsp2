82fde74901bac06ab7b9c4f0af1983ac069ad5ae
Pedro Costa <pedrocosta@infinitygames.io>
2023-05-22 17:58:16 +0100

    Added BepInEx Assemblies to User Assemblies

---
+++ UitkForKsp2Project/Patch/RegisterBepInExAssembliesPatch.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx.Bootstrap;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace UitkForKsp2.Patch;
+
+#pragma warning disable CS1591
+public static class RegisterBepInExAssembliesPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ScriptingRuntime), nameof(ScriptingRuntime.GetAllUserAssemblies))]
+    public static void RegisterBepInExAssemblies(ref string[] __result)
+    {
+        __result = __result.AddRangeToArray(Chainloader.Plugins.SelectMany(plugin => plugin.GetType().Assembly.GetFiles())
+            .Select(file => Path.GetFileNameWithoutExtension(file.Name)).ToArray());
+    }
+}
