aa7a0f6b564f120d11e001e9c9b50ae1bb9df222
Jan Bureš <jan.bures@live.com>
2023-06-11 00:30:01 +0200

    Patcher IDE attributes

--- src/UitkForKsp2.Patcher/Patcher.cs
+++ src/UitkForKsp2.Patcher/Patcher.cs
@@ -1,12 +1,14 @@
-﻿using UitkForKsp2;
+﻿using JetBrains.Annotations;
 using Mono.Cecil;
 
-namespace UitkForKsp2Patcher;
+namespace UitkForKsp2.Patcher;
 
+[UsedImplicitly]
 public static class Patcher
 {
     private static readonly string BasePath = Path.Combine("BepInEx", "plugins", UitkForKsp2Plugin.ModGuid, "lib");
 
+    [UsedImplicitly]
     public static IEnumerable<string> TargetDLLs { get; } = new[]
     {
         "UnityEngine.TextCoreTextEngineModule.dll",
@@ -17,6 +19,7 @@ public static class Patcher
         "UnityEngine.UIModule.dll"
     };
 
+    [UsedImplicitly]
     public static void Patch(ref AssemblyDefinition assembly)
     {
         assembly = AssemblyDefinition.ReadAssembly(Path.Combine(BasePath, $"{assembly.Name.Name}.dll"));
