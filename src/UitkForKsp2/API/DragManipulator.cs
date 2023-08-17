70b45f6e7aac0489e6b8c1f657f6aaa0fc9f3e47
Jan Bureš <jan.bures@live.com>
2023-08-18 00:47:17 +0200

    DragManipulator: added enabling/disabling and screen-bound dragging constraints

--- src/UitkForKsp2/API/DragManipulator.cs
+++ src/UitkForKsp2/API/DragManipulator.cs
@@ -19,6 +19,16 @@ public class DragManipulator : IManipulator
     public bool Dragging { get; set; }
 
     /// <summary>
+    /// Is dragging currently enabled?
+    /// </summary>
+    public bool IsEnabled { get; set; }
+
+    /// <summary>
+    /// Allow dragging off screen?
+    /// </summary>
+    public bool AllowDraggingOffScreen { get; set; }
+
+    /// <summary>
     /// The target element to drag.
     /// </summary>
     public VisualElement target
@@ -32,6 +42,15 @@ public class DragManipulator : IManipulator
             _target.RegisterCallback<PointerUpEvent>(DragEnd);
             _target.RegisterCallback<PointerMoveEvent>(PointerMove);
         }
+    }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="DragManipulator"/> class.
+    /// </summary>
+    /// <param name="allowDraggingOffScreen">Allow dragging off screen?</param>
+    public DragManipulator(bool allowDraggingOffScreen = true)
+    {
+        AllowDraggingOffScreen = allowDraggingOffScreen;
     }
 
     private void DragBegin(PointerDownEvent evt)
@@ -58,12 +77,20 @@ public class DragManipulator : IManipulator
 
     private void PointerMove(PointerMoveEvent evt)
     {
-        if (!Dragging)
+        if (!IsEnabled || !Dragging)
         {
             return;
         }
 
         var delta = evt.localPosition - _offset;
-        target.transform.position += delta;
+        var newPosition = target.transform.position + delta;
+
+        if (!AllowDraggingOffScreen)
+        {
+            newPosition.x = Mathf.Clamp(newPosition.x, 0, Screen.width - target.resolvedStyle.width);
+            newPosition.y = Mathf.Clamp(newPosition.y, 0, Screen.height - target.resolvedStyle.height);
+        }
+
+        target.transform.position = newPosition;
     }
 }
