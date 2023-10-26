c5ed2e5ff6d89dddb5ebff1abcca6e65eb8cbd78
Jan Bureš <jan.bures@live.com>
2023-10-26 02:54:36 +0200

    Dirty hack to fix DragManipulator interactions with TextFields

--- src/UitkForKsp2/API/DragManipulator.cs
+++ src/UitkForKsp2/API/DragManipulator.cs
@@ -1,35 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.UIElements;
 
-namespace UitkForKsp2.API;
-
 /// <summary>
-/// A simple drag manipulator for UIElements.
+/// A manipulator to make UI Toolkit elements draggable within the screen bounds.
 /// </summary>
-[PublicAPI]
 public class DragManipulator : IManipulator
 {
     private VisualElement _target;
-    private Vector3 _offset;
+    private Vector2 _offset;
     private PickingMode _mode;
+    private IVisualElementScheduledItem _scheduledItem;
 
     /// <summary>
-    /// Is the target currently being dragged?
+    /// Indicates whether the element is currently being dragged.
     /// </summary>
-    public bool Dragging { get; set; }
+    public bool IsDragging { get; private set; }
 
     /// <summary>
-    /// Is dragging currently enabled?
+    /// Enables or disables the dragging functionality.
     /// </summary>
-    public bool IsEnabled { get; set; }
+    public bool IsEnabled { get; set; } = true;
 
     /// <summary>
-    /// Allow dragging off screen?
+    /// Indicates whether the element can be dragged off screen.
     /// </summary>
     public bool AllowDraggingOffScreen { get; set; }
 
     /// <summary>
-    /// The target element to drag.
+    /// The target element that will be made draggable.
     /// </summary>
     public VisualElement target
     {
@@ -37,10 +35,9 @@ public class DragManipulator : IManipulator
         set
         {
             _target = value;
-
-            _target.RegisterCallback<PointerDownEvent>(DragBegin);
-            _target.RegisterCallback<PointerUpEvent>(DragEnd);
-            _target.RegisterCallback<PointerMoveEvent>(PointerMove);
+            _target.RegisterCallback<PointerDownEvent>(OnPointerDown);
+            _target.RegisterCallback<PointerMoveEvent>(OnPointerMove);
+            _target.RegisterCallback<PointerUpEvent>(OnPointerUp);
         }
     }
 
@@ -48,14 +45,17 @@ public class DragManipulator : IManipulator
     /// Creates a new instance of the <see cref="DragManipulator"/> class.
     /// </summary>
     /// <param name="allowDraggingOffScreen">Allow dragging off screen?</param>
-    public DragManipulator(bool allowDraggingOffScreen = true)
+    public DragManipulator(bool allowDraggingOffScreen = false)
     {
         AllowDraggingOffScreen = allowDraggingOffScreen;
     }
 
-    private void DragBegin(PointerDownEvent evt)
+    /// <summary>
+    /// Handles the initiation of the dragging process.
+    /// </summary>
+    private void OnPointerDown(PointerDownEvent evt)
     {
-        if (evt.target is TextField.TextInput)
+        if (!IsEnabled || evt.target is TextField.TextInput)
         {
             evt.StopPropagation();
             return;
@@ -63,34 +63,54 @@ public class DragManipulator : IManipulator
 
         _mode = target.pickingMode;
         target.pickingMode = PickingMode.Ignore;
+        IsDragging = true;
         _offset = evt.localPosition;
-        Dragging = true;
-        target.CapturePointer(evt.pointerId);
+        _target.CapturePointer(evt.pointerId);
+
+        // Schedule a recurring check for the mouse button state
+        _scheduledItem = _target.schedule.Execute(CheckMouseButtonState).Every(100);
     }
 
-    private void DragEnd(PointerUpEvent evt)
+    private void CheckMouseButtonState()
     {
-        target.ReleasePointer(evt.pointerId);
-        Dragging = false;
-        target.pickingMode = _mode;
+        if (!Input.GetMouseButton(0))
+        {
+            OnPointerUp(null);
+        }
     }
 
-    private void PointerMove(PointerMoveEvent evt)
+    /// <summary>
+    /// Handles the movement of the draggable element.
+    /// </summary>
+    private void OnPointerMove(PointerMoveEvent evt)
     {
-        if (!IsEnabled || !Dragging)
+        if (!IsDragging || !IsEnabled)
         {
             return;
         }
 
-        var delta = evt.localPosition - _offset;
-        var newPosition = target.transform.position + delta;
+        var newPosition = new Vector2(evt.position.x, evt.position.y) - _offset;
 
         if (!AllowDraggingOffScreen)
         {
-            newPosition.x = Mathf.Clamp(newPosition.x, 0, Screen.width - target.resolvedStyle.width);
-            newPosition.y = Mathf.Clamp(newPosition.y, 0, Screen.height - target.resolvedStyle.height);
+            newPosition.x = Mathf.Clamp(newPosition.x, 0, Screen.width - _target.resolvedStyle.width);
+            newPosition.y = Mathf.Clamp(newPosition.y, 0, Screen.height - _target.resolvedStyle.height);
         }
 
-        target.transform.position = newPosition;
+        _target.transform.position = new Vector3(newPosition.x, newPosition.y, _target.transform.position.z);
+    }
+
+    /// <summary>
+    /// Handles the end of the dragging process.
+    /// </summary>
+    private void OnPointerUp(PointerUpEvent evt)
+    {
+        IsDragging = false;
+        _target.ReleasePointer(evt.pointerId);
+        target.pickingMode = _mode;
+
+        // Cancel the scheduled mouse button state check
+        _scheduledItem?.Pause();
+        _scheduledItem = null;
     }
 }
